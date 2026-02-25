ce963758b081cff624aab15ff5d0471da8de7490
yrwq <yrwqid@gmail.com>
2026-02-25 19:57:24 +0100

    docs: update

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-f970c26f.js',
+                path_to_root + 'searchindex-2ee0de41.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
