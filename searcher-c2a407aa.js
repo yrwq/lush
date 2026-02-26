fbb5a9a32a1cb5ed1ce7620d95c299afc0ee5def
yrwq <yrwqid@gmail.com>
2026-02-26 19:39:08 +0100

    docs: add tray

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-2ee0de41.js',
+                path_to_root + 'searchindex-ffd1b501.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
