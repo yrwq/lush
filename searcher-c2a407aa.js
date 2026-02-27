e8131e09d96855b35a1a30fff8da955f1af8b8a8
yrwq <yrwqid@gmail.com>
2026-02-27 21:15:55 +0100

    docs: add osd

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-ffd1b501.js',
+                path_to_root + 'searchindex-bbe08d1e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
